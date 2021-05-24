4f993869289956835293ef26e7291f940a469c92
merisimo13 <simonishvili.m13@gmail.com>
2021-05-24 12:53:25 +0400

    new version

--- src/test/java/Balance.java
+++ src/test/java/Balance.java
@@ -9,30 +9,20 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.sleep;
 
-public class PortalLogIn {
+public class Balance {
     @Test
-    public void firstTask (){
+    public void secondtask(){
         Configuration.browser = "chrome";
         System.setProperty("webdriver.chrome.driver", "files/chromedriver.exe");
         Configuration.startMaximized = true;
 
         Selenide.open("https://my.silknet.com/login");
         $(byName("_username")).setValue("msimonishvili@silknet.com");
-        $(byName("_password")).setValue("Paroli18");
-        $(byName("_submit")).click();
-        $(byText("ელფოსტა ან პაროლი არასწორია")).shouldBe(Condition.visible);
         $(byName("_password")).setValue("Paroli13");
-        $(byText("ელფოსტა ან პაროლი არასწორია")).shouldNotBe(Condition.visible);
         $(byName("_submit")).click();
-        $(".logo_img").waitUntil(Condition.visible, 3000);
 
 
         sleep (5000);
 
-
-
-
-
     }
-
 }
